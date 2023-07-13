b75aab392430c313d10e270efd6c17cc5bf0f3fb
aarnavi <130127222+aarnavicodes@users.noreply.github.com>
2023-07-13 15:43:50 +0800

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor(x, y, width, height){
+        var options ={
+            isStatic: true
+        };
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+    }
+
+    display(){
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
